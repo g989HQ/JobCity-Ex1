b64628810c8a96e1b1f16eb876feadb86fa0c60e
g989HQ <44266655+g989HQ@users.noreply.github.com>
2022-02-05 20:59:37 -0300

    Push - Automation tests for Interacting with Tabs and Send Feedback

---
+++ Activity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class Activity
+{
+
+
+	public static void WaitForElementbyID(IWebDriver driver, string id, int maxwait = 30000)
+	{
+		//IWebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(maxwait));
+
+		
+			try
+			{
+				//var element = driver.FindElement(By.Id(id));
+				var element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(id)));
+			}
+
+			catch { }
+		
+	
+	}
+
+	public static bool WaitForElementbyXpath(IWebDriver driver, string xpath, int maxwait = 30000)
+	{
+		//IWebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(maxwait));
+
+	 
+			try
+			{
+				//var element = driver.FindElement(By.Id(id));
+				var element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(xpath)));
+			    return true;
+			}
+
+			catch {
+
+			return false;
+		
+		}
+		
+		
+
+	}
+
+	public static void NavigateTabs(IWebDriver driver, string id, string expecteddisplay)
+    {
+		try
+		{
+			IWebElement searchitem = driver.FindElement(By.Id(id));
+			if (searchitem.Displayed)
+			{
+				searchitem.Click();
+				Activity.WaitForElementbyXpath(driver,expecteddisplay);
+			}
+
+
+		}
+		catch (NoSuchElementException)
+		{
+			System.Console.WriteLine("Element or button not found");
+		}
+
+
+
+	}
+
+	public static void ClickbuttonById(IWebDriver driver, string id)
+	{
+		try
+		{
+
+			IWebElement element = driver.FindElement(By.Id(id));
+
+			element.Click();
+		}
+
+        catch 
+        {
+			System.Console.WriteLine("Element or button not found");
+		}
+	}
+
+	public static void ClickbuttonByXpath(IWebDriver driver, string xpath)
+	{
+		try
+		{
+
+			IWebElement element = driver.FindElement(By.XPath(xpath));
+
+			element.Click();
+		}
+
+		catch
+		{
+			System.Console.WriteLine("Element or button not found");
+		}
+	}
+
+
+
+	public static void ClickbuttonByName(IWebDriver driver, string name)
+	{
+		try
+		{
+
+			IWebElement element = driver.FindElement(By.Name(name));
+
+			element.Click();
+		}
+
+		catch
+		{
+			System.Console.WriteLine("Element or button not found");
+		}
+	}
+
+
+	public static void SendKeysById(IWebDriver driver, string id, string text)
+	{
+		try
+		{
+
+			IWebElement element = driver.FindElement(By.Id(id));
+			element.SendKeys(text);
+		}
+
+		catch
+		{
+			System.Console.WriteLine("Element or button not found");
+		}
+	}
+
+
+
+}
---
+++ ZeroWebAppTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace ZeroWebApp_Ex1
+{
+    class ZeroWebAppTest
+    {
+        [TestFixture]
+
+        public class ZeroWebbTests
+
+        {
+            [Test]
+            public void NavigateAllTabs()
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("http://zero.webappsecurity.com");
+                driver.Manage().Window.Maximize();
+                //Time added to follow up execution easier
+                Thread.Sleep(5000);
+                Activity.WaitForElementbyXpath(driver, "//a[contains(.,'Zero Bank')]");
+                //Time added to follow up execution easier
+                Thread.Sleep(5000);
+                Activity.NavigateTabs(driver, "onlineBankingMenu", "//span[contains(.,'Account Summary')]");
+                //Time added to follow up execution easier
+                Thread.Sleep(5000);
+                Activity.NavigateTabs(driver, "feedback", "//span[contains(.,'Frequently Asked Questions')]");
+                Activity.WaitForElementbyXpath(driver, "//a[contains(.,'Zero Bank')]");
+                Thread.Sleep(6000);
+
+                Activity.ClickbuttonByXpath(driver, "//a[starts-with(.,'Zer')]");
+              //  driver.Navigate().GoToUrl("http://zero.webappsecurity.com/index.html");
+                Thread.Sleep(6000);
+                Activity.WaitForElementbyXpath(driver, "//img[starts-with(@src,'/resources/img/main_carousel_1.jpg')]");
+                Thread.Sleep(6000);
+                driver.Quit();
+
+
+            }
+
+            [Test]
+            public void SendFeedback()
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("http://zero.webappsecurity.com");
+                driver.Manage().Window.Maximize();
+                //Time added to follow up execution easier
+                Thread.Sleep(5000);
+                Activity.WaitForElementbyXpath(driver, "//a[contains(.,'Zero Bank')]");             
+                Activity.NavigateTabs(driver, "feedback", "//span[contains(.,'Frequently Asked Questions')]");
+                Activity.WaitForElementbyXpath(driver, "//a[contains(.,'Zero Bank')]");
+                Thread.Sleep(6000);
+                Activity.SendKeysById(driver, "name", "Auto");
+                Activity.SendKeysById(driver, "email", "Auto");
+                Activity.SendKeysById(driver, "subject", "Auto");
+                Activity.SendKeysById(driver, "comment", "Auto");
+                Thread.Sleep(3000);
+                Activity.ClickbuttonByName(driver, "submit");
+
+                string feedbacksent = "Feedback\r\nThank you for your comments, Auto. They will be reviewed by our Customer Service staff and given the full attention that they deserve.";
+
+                IWebElement element = driver.FindElement(By.XPath("//div[contains(@class,'offset3 span6')]"));
+
+                var text = element.Text;
+
+                if (feedbacksent == text)
+                {
+                    Assert.Fail("The feedback shouldn't be sent, the fields need to be validated.");
+                }
+
+                driver.Quit();
+
+
+            }
+
+            [TearDown]
+            public void testClean()
+            {
+
+            }
+        }
+    }
+}
